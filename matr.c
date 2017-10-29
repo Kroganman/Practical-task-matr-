2c3610e24e7f3b3c7bcd9f9546d362ac04f5f9fb
Kroganman <31574752+Kroganman@users.noreply.github.com>
2017-10-29 22:12:21 +0300

    Update matr.c
    Fixed matrix multiplication error

--- matr.c
+++ matr.c
@@ -12,11 +12,11 @@ m=rand() %5 +3;
 n=rand() %5 +4;
 printf("Strings:%d, Columns:%d\n\n",m,n);
 int mas[m][n];
-for(i=1;i<m+1;i++)
+for(i=0;i<m;i++)
 {
-    for(j=1;j<n+1;j++)
+    for(j=0;j<n;j++)
     {
-        mas[i][j]=-500 + rand() %1000;
+        mas[i][j]=-50 + rand() %100;
         printf("%d ",mas[i][j]);
     }
     printf("\n");
@@ -25,15 +25,15 @@ printf("\n\n\n");
 /*-----------------------------------------*/
 /* 2 exercise */
 int k;
-for(j=1;j<n+1;j++)
+for(j=0;j<n;j++)
 {
-    k=mas[1][j];
-    mas[1][j]=mas[m][n-j];
-    mas[m][n-j]=k;
+    k=mas[0][j];
+    mas[0][j]=mas[m-1][n-j-1];
+    mas[m-1][n-j-1]=k;
 }
-for(i=1;i<m+1;i++)
+for(i=0;i<m;i++)
 {
-    for(j=1;j<n+1;j++)
+    for(j=0;j<n;j++)
     {
         printf("%d ",mas[i][j]);
     }
@@ -45,9 +45,9 @@ printf("\n\n\n");
 int mas2[m-1][n-1];
 int min, min_str, min_col,x,y;
 min=501;
-for(i=1;i<m+1;i++)
+for(i=0;i<m;i++)
 {
-    for(j=1;j<n+1;j++)
+    for(j=0;j<n;j++)
        {
 
           if(mas[i][j]<min)
@@ -59,15 +59,15 @@ for(i=1;i<m+1;i++)
        }
 }
 printf("Min. element = %d, String = %d, Column = %d\n\n", min, min_str, min_col);
-i=1;
-j=1;
-x=1;
-y=1;
-while(i<m+1)
+i=0;
+j=0;
+x=0;
+y=0;
+while(i<m)
 {
     if(i!=min_str)
     {
-       while(j<n+1)
+       while(j<n)
        {
            if(j!=min_col)
            {
@@ -89,19 +89,20 @@ while(i<m+1)
     }
     x++;
     i++;
-    j=1;
+    j=0;
+    y=0;
 }
 m--;
 n--;
 printf("\n\n\n");
 /*--------------------------------------------*/
 /* 4 exercise */
-int new_mas[8][9];
+int new_mas[9][10];
 char a;
 int b;
 b=0;
-i=1;
-j=1;
+i=0;
+j=0;
 while((a=getchar()) !='.')
 {
     if(a>='0' && a<='9')
@@ -119,26 +120,26 @@ while((a=getchar()) !='.')
         printf("%d\n",new_mas[i][j]);
         b=0;
         i++;
-        j=1;
+        j=0;
     }
 }
 printf("\n\n\n");
 /*-------------------------------------------*/
 /* 5 exercise*/
-if(n!=i)
+if(n!=i+1)
 {
     printf("The number of columns should equal the number of rows of the old matrix!");
     return 1;
 }
 int z,q;
-z=j++;
+z=j;
 int mas_mas[m][z];
-for(i=1;i<m+1;i++)
+for(i=0;i<m;i++)
 {
-    for(j=1;j<z+1;j++)
+    for(j=0;j<z;j++)
     {
         mas_mas[i][j]=0;
-       for(q=1;q<n+1;q++)
+       for(q=0;q<n;q++)
        {
            mas_mas[i][j]=mas_mas[i][j]+mas2[i][q]*new_mas[q][j];
        }
